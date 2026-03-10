ffa7849dcc924ac6a1c7d14e5a5d24c18bae3611
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2026-03-10 14:51:08 +1000

    Ajj Surprisingly no roza lagging......

---
+++ Rudiments-Practice/IntroductionToPython-Lab00/StringSlicing.py
@@ -0,0 +1,28 @@
+# Man this reminds me of Sir Minhas ki class in A2 Nix 
+# the extra class in Audi (Sir Naushad ka room), Revision Class Merged for both CMp 204 & 205
+# 2 weeks before p4 practical, and lol no one could recall this ke yeh sir ne prhaya tha 
+# sir was stunned!!
+
+t = 'Assalam u Alaikum Duniya!!!'
+
+#prints entire string 
+print(t) 
+
+#prints first character of the string  
+print(t[0])
+
+#prints character starting from the 3rd character to the 5th, indexed starting from 1st character 
+#or we could say that 
+#gives us the character from 2nd character to the 4th character (n - 1) from the upper boundary, indexed from the first character
+print(t[2:5])
+
+#prints characters starting from the 2nd character, idexed characters start from 0 
+#or we could say that 
+#prints characters starting from the 3rd charcater, string is indexed from 1 
+print(t[2:])
+
+#prints the string 2 times
+print(t*2)
+
+#prints the string in t with the concatenated pattern of 'TEST'
+print(t + 'TEST')
